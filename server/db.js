3419a0e35e65dbd928a4812e19e679ba5c67898f
NayanaKamtekar <nayana.kamtekar@gmail.com>
2021-06-09 00:16:45 +0200

    Added .env example

--- server/db.js
+++ server/db.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1814Nkk",
-  database: "proposalsdb",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
-
 
 pool.getConnection((err, connection) => {
   if (err) {
